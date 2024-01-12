5ffecf0bc1d696930f1e7a0528eac6a912c0002c
k神 <156385827+kk112331@users.noreply.github.com>
2024-01-12 09:27:08 +0800

    Add files via upload

---
+++ 极光.sh
@@ -0,0 +1,83 @@
+        sleep 1.3s
+echo -e '\033[31m 【★极光端口★专用和平护航脚本，正在开启服务】\031[0m'
+sleep 1.3s
+         echo -e '\033[31m 【一】\033[0m'
+sleep 1.3s
+         echo -e '\033[31m 【二】\033[0m'
+sleep 1.3s
+         echo -e '\033[31m 【三】\033[0m'
+sleep 1.3s
+         echo -e '\033[31m 【四】\033[0m'
+sleep 1.3s
+         echo -e '\033[31m 【五】\033[0m'
+sleep 1.3s
+         echo -e '\033[31m 【六】\033[0m'
+sleep 1.3s
+while true
+do
+rm -rf /data/user/0/com.tencent.tmgp.pubgmhd/files/ano_tmp/dec_fpv.zip
+rm -rf /data/user/0/com.tencent.tmgp.pubgmhd/files/ano_tmp/tss_cfg2.dat
+rm -rf /data/user/0/com.tencent.tmgp.pubgmhd/files/ano_tmp/tss_base.dat
+rm -rf /data/user/0/com.tencent.tmgp.pubgmhd/files/ano_tmp/tss_gp5.dat
+rm -rf /data/user/0/com.tencent.tmgp.pubgmhd/files/ano_tmp/tss_tcj.dat
+rm -rf /data/user/0/com.tencent.tmgp.pubgmhd/files/ano_tmp/mrpcs.data
+rm -rf /data/user/0/com.tencent.tmgp.pubgmhd/files/ano_tmp/ob_normal.zip
+rm -rf /data/user/0/com.tencent.tmgp.pubgmhd/files/ano_tmp/tssmua.zip
+rm -rf /data/user/0/com.tencent.tmgp.pubgmhd/files/ano_tmp/tssmua_once.zip
+rm -rf /data/user/0/com.tencent.tmgp.pubgmhd/files/ano_tmp/comm.dat
+rm -rf /data/user/0/com.tencent.tmgp.pubgmhd/files/ano_tmp/comm.zip
+rm -rf /data/user/0/com.tencent.tmgp.pubgmhd/files/ano_tmp/ob_x.zip
+rm -rf /data/user/0/com.tencent.tmgp.pubgmhd/files/ano_tmp/custom_cache/*
+rm -rf /data/data/com.tencent.tmgp.pubgmhd/files/ano_tmp/dec_fpv.zip
+rm -rf /data/data/com.tencent.tmgp.pubgmhd/files/ano_tmp/tss_cfg2.dat
+rm -rf /data/data/com.tencent.tmgp.pubgmhd/files/ano_tmp/tss_base.dat
+rm -rf /data/data/com.tencent.tmgp.pubgmhd/files/ano_tmp/tss_gp5.dat
+rm -rf /data/data/com.tencent.tmgp.pubgmhd/files/ano_tmp/tss_tcj.dat
+rm -rf /data/data/com.tencent.tmgp.pubgmhd/files/ano_tmp/mrpcs.data
+rm -rf /data/data/com.tencent.tmgp.pubgmhd/files/ano_tmp/ob_normal.zip
+rm -rf /data/data/com.tencent.tmgp.pubgmhd/files/ano_tmp/tssmua.zip
+rm -rf /data/data/com.tencent.tmgp.pubgmhd/files/ano_tmp/tssmua_once.zip
+rm -rf /data/data/com.tencent.tmgp.pubgmhd/files/ano_tmp/comm.dat
+rm -rf /data/data/com.tencent.tmgp.pubgmhd/files/ano_tmp/comm.zip
+rm -rf /data/data/com.tencent.tmgp.pubgmhd/files/ano_tmp/ob_x.zip
+rm -rf /data/data/com.tencent.tmgp.pubgmhd/files/ano_tmp/custom_cache/*
+rm -rf /data/user/0/com.tencent.tmgp.pubgmhd/files/ano_tmp
+rm -rf /data/data/com.tencent.tmgp.pubgmhd/files/ano_tmp
+
+echo "第一阶段游戏服务开启"
+echo "准备开启第二阶段"
+
+chmod 555 /data/user/0/com.tencent.tmgp.pubgmhd/files/ano_tmp
+chmod 555 /data/data/com.tencent.tmgp.pubgmhd/files/ano_tmp
+chmod 555 /data/data/com.tencent.tmgp.pubgmhd/files/tss_tmp
+chmod 555 /data/user/0/com.tencent.tmgp.pubgmhd/files/tss_tmp
+
+echo "第三阶段服务已开启"
+echo "全部服务已开启"
+
+    sleep 3s
+    set +x
+    let "j=j+1"
+    echo -e "\033[32;;1m 极光端口游戏护航引擎sh \033[0m" 
+    echo "服务正常\033[0m"
+    echo -e "\033[32;;1m 服务正常 \033[0m" 
+    echo -e "\033[32;;1m 服务正常 \033[0m" 
+    echo -e "\033[32;;1m 服务正常 \033[0m" 
+    echo -e "\033[32;;1m 服务正常 \033[0m" 
+    echo -e "\033[32;;1m 服务正常 \033[0m" 
+    echo -e "\033[32;;1m 服务正常 \033[0m" 
+    echo -e "\033[32;;1m 服务正常 \033[0m" 
+    echo -e "\033[32;;1m 服务正常 \033[0m" 
+    echo -e "\033[32;;1m 服务正常 \033[0m" 
+        echo -e "\033[32;;1m 服务正常 \033[0m" 
+            echo -e "\033[32;;1m 服务正常 \033[0m" 
+                echo -e "\033[32;;1m 服务正常 \033[0m" 
+                    echo -e "\033[32;;1m 服务正常 \033[0m" 
+                        echo -e "\033[32;;1m 服务正常 \033[0m" 
+        echo -e "\033[32;;1m 服务正常 \033[0m" 
+            echo -e "\033[32;;1m 服务正常 \033[0m" 
+                echo -e "\033[32;;1m 服务正常 \033[0m" 
+    
+    
+    echo -e "\033[5;46;42;37m  循环：$j 次  \033[0m" 
+done
